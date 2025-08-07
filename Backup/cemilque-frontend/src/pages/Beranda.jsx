6e727cd81edaaafccfa19a0c41459e05087bfef2
Darryl Satria <bimdai0304@gmail.com>
2025-08-07 14:43:05 +0700

    Delete Backup/cemilque-frontend directory

--- Backup/cemilque-frontend/src/pages/Beranda.jsx
+++
@@ -1,9 +0,0 @@
-const Beranda = () => {
-  return (
-    <div>
-        <h1 className="black-300 font-bold">Hello From Dashboard</h1>
-    </div>
-  )
-}
-
-export default Beranda
--- Backup/cemilque-frontend/src/pages/Penyimpanan.jsx
+++
@@ -1,9 +0,0 @@
-const Penyimpanan = () => {
-  return (
-    <div>
-        <h1 className="black-300 font-bold">Hello From Penyimpanan</h1>
-    </div>
-  )
-}
-
-export default Penyimpanan
